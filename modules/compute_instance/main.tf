b7857feaeb7ef38a9bf921d23941b0f35d3e72eb
Sahil Pawar <sahil.pawar@incentius.com>
2022-05-26 18:21:51 +0530

    vvv

--- modules/compute_instance/main.tf
+++ modules/compute_instance/main.tf
@@ -2,4 +2,16 @@ resource "google_compute_instance" "default" {
   name         = var.name
   machine_type = var.machine_type
   zone         = var.zone
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
 }
