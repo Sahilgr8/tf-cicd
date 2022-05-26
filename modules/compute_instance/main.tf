711c86f6154575c14ecf4e1dedca05c2d06e8b21
Sahil Pawar <sahil.pawar@incentius.com>
2022-05-26 18:30:04 +0530

    fail feature

--- modules/compute_instance/main.tf
+++ modules/compute_instance/main.tf
@@ -2,11 +2,11 @@ resource "google_compute_instance" "default" {
   name         = var.name
   machine_type = var.machine_type
   zone         = var.zone
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-9"
+#     }
+#   }
   network_interface {
     network = "default"
 
